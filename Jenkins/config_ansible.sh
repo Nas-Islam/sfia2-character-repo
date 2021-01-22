4269b3742d9ceb097e3957494e1daaf3bb2cdbf4
Nas-Islam <islam626@hotmail.com>
2021-01-22 12:09:43 +0000

    Adjusted Jenkinsfile to include ansible config file

--- Jenkins/config_ansible.sh
+++ Jenkins/config_ansible.sh
@@ -5,5 +5,5 @@ mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 pip3 install --user ansible
-cd ansible
+cd ansible-swarm
 /home/jenkins/.local/bin/ansible-playbook -i inventory playbook.yaml
