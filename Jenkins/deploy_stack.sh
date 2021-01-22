42777159cb60ee6aa97d982d18189d77775ab3bd
Nas-Islam <islam626@hotmail.com>
2021-01-22 13:11:03 +0000

    Updated deploy_stack

--- Jenkins/deploy_stack.sh
+++ Jenkins/deploy_stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml character-generator
