248c3c42d1df9c929e860475799ffcf23eb7f41d
Nas-Islam <islam626@hotmail.com>
2021-01-23 23:59:52 +0000

    Changing Image Links to Nexus Repository

--- Jenkins/deploy_stack.sh
+++ Jenkins/deploy_stack.sh
@@ -2,9 +2,9 @@
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
-    export AUTHOR=${AUTHOR}
-    export app_version = 'v2'
+    export REPO_IP=${REPO_IP}
+    export app_version = 'v1'
     export rollback = 'false'
-    export replicas = 10
+    export replicas = 5
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml character-generator
 EOF
